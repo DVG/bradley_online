98f66fefb09844b89c2a36b4849550276cad755a
DVG <devryguy@gmail.com>
2012-12-05 16:02:10 -0500

    Prettifying code blocks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 module ApplicationHelper
+  class HTMLWithPrettyPrint < Redcarpet::Render::HTML
+    def block_code(code, language)
+      "<pre class=\"prettyprint linenums\">#{code}</pre>"
+    end
+  end
+  
   def markdown(text)
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :fenced_code_blocks => true)
+    markdown = Redcarpet::Markdown.new(HTMLWithPrettyPrint, :fenced_code_blocks => true)
     markdown.render(text).html_safe
   end
 end
